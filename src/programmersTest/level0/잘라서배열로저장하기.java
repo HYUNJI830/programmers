da92699bcfdedabf47b7702546ad0278b4e953f7
HYUNJI830 <hyun_ji830@naver.com>
2023-01-09 19:58:43 +0900

    LV0

--- src/programmersTest/level0/잘라서배열로저장하기.java
+++ src/programmersTest/level0/잘라서배열로저장하기.java
@@ -1,6 +1,5 @@
 package programmersTest.level0;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class 잘라서배열로저장하기 {
@@ -10,6 +9,25 @@ public class 잘라서배열로저장하기 {
 		int n = 6;
 		// 답 :["abc1Ad", "dfggg4", "556b"]
 	
+		int A = (my_str.length() + n - 1) / n;
+		String[] answer = new String[A];
+		
+		for(int i=0; i<A; i++) {
+			int start = n*i;
+			//0,6,12
+			int end = 0;
+				if(start + n >=my_str.length()) {
+					end = my_str.length();
+					
+				} else {
+					end = start + n;
+					//6,12
+				}
+				answer[i] = my_str.substring(start,end);
+		}
+		
+		System.out.println(Arrays.toString(answer));
+		//[abc1Ad, dfggg4, 556b]
 	}
 
 }
